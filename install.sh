cac6a23d98b5dfcb5ea04320ecdcf80ae9fc98a6
Trick van Staveren <trick@vanstaveren.us>
2007-07-02 16:57:41 +0000

    use nicer output for backup/install output
    git-svn-id: http://svn.vanstaveren.us/trick_skel@175 434ebc3a-ad12-0410-8bf5-d84fc7825958

--- install.sh
+++ install.sh
@@ -6,20 +6,24 @@ INSTALL_DOTFILES=".screenrc .vimrc .bash_profile_trick_skel"
 
 mkdir -p $HOME/tmp/trick_skel_backup
 
+echo -n "Backing up "
 for cur in $INSTALL_DOTFILES; do
     pushd $HOME > /dev/null
-    echo "Backing up $cur..."
+    echo -n "$cur, "
     cp $cur $HOME/tmp/trick_skel_backup
     popd > /dev/null
 done
+echo
 
 tar cfj $HOME/tmp/trick_skel_backup-`date -u "+%Y%m%d-%H%M%S"`.tar.bz2 $HOME/tmp/trick_skel_backup > /dev/null
 rm -rf $HOME/tmp/trick_skel_backup
 
+echo -n "Installing "
 for cur in $INSTALL_DOTFILES; do
-    echo "Installing $cur..."
+    echo -n "$cur, "
     cp skel/$cur $HOME/$cur
 done
+echo
 
 grep bash_profile_trick_skel $HOME/.bash_profile &> /dev/null
 if [ $? -ne 0 ]; then
