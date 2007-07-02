6ef360433e013551f745dd874e54f5435fe9783f
Trick van Staveren <trick@vanstaveren.us>
2007-07-02 16:40:29 +0000

    modify install script, and fix syntax errors in bash_profile
    git-svn-id: http://svn.vanstaveren.us/trick_skel@173 434ebc3a-ad12-0410-8bf5-d84fc7825958

--- install.sh
+++ install.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
-mkdir -p ~/tmp/trickv_skel_backup
+INSTALL_DOTFILES=".screenrc .vimrc .bash_profile_trick_skel"
+
+#####
+
+mkdir -p ~/tmp/trick_skel_backup
 
 for cur in $INSTALL_DOTFILES; do
-    pushd ~/
+    pushd ~/ > /dev/null
     echo "Backing up $cur..."
-    cp $cur ~/tmp/trickv_skel_backup
-    popd
+    cp $cur ~/tmp/trick_skel_backup
+    popd > /dev/null
 done
 
-tar cfj ~/tmp/trickv_skel_backup-`date +%Y%m%d-%H%M%S -u` ~/tmp/trickv_skel_backup
-rm -rf ~/tmp/trickv_skel_backup
+tar cfj ~/tmp/trick_skel_backup-`date +%Y%m%d-%H%M%S -u`.tar.bz2 ~/tmp/trick_skel_backup > /dev/null
+rm -rf ~/tmp/trick_skel_backup
 
 for cur in $INSTALL_DOTFILES; do
     echo "Installing $cur..."
-    cp $cur ~/${cur}
+    cp skel/$cur ~/$cur
 done
 
-grep bash_profile_trickv_skel $HOME/.bash_profile &> /dev/null
+grep bash_profile_trick_skel $HOME/.bash_profile &> /dev/null
 if [ $? -ne 0 ]; then
     echo "Installing bash_profile hook..."
     echo "
-# trickv's bash_profile script
-source ~/.bash_profile_trickv_skel" >> ~/.bash_profile
+# trick's bash_profile script
+source ~/.bash_profile_trick_skel" >> ~/.bash_profile
 fi
 
 echo "Done."
