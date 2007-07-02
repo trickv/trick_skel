f7a4b7c525baeb20104927708259864a28592a3c
Trick van Staveren <trick@vanstaveren.us>
2007-07-02 16:56:44 +0000

    make script happy on FreeBSD and use env to find bash
    git-svn-id: http://svn.vanstaveren.us/trick_skel@174 434ebc3a-ad12-0410-8bf5-d84fc7825958

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 INSTALL_DOTFILES=".screenrc .vimrc .bash_profile_trick_skel"
 
 #####
 
-mkdir -p ~/tmp/trick_skel_backup
+mkdir -p $HOME/tmp/trick_skel_backup
 
 for cur in $INSTALL_DOTFILES; do
-    pushd ~/ > /dev/null
+    pushd $HOME > /dev/null
     echo "Backing up $cur..."
-    cp $cur ~/tmp/trick_skel_backup
+    cp $cur $HOME/tmp/trick_skel_backup
     popd > /dev/null
 done
 
-tar cfj ~/tmp/trick_skel_backup-`date +%Y%m%d-%H%M%S -u`.tar.bz2 ~/tmp/trick_skel_backup > /dev/null
-rm -rf ~/tmp/trick_skel_backup
+tar cfj $HOME/tmp/trick_skel_backup-`date -u "+%Y%m%d-%H%M%S"`.tar.bz2 $HOME/tmp/trick_skel_backup > /dev/null
+rm -rf $HOME/tmp/trick_skel_backup
 
 for cur in $INSTALL_DOTFILES; do
     echo "Installing $cur..."
-    cp skel/$cur ~/$cur
+    cp skel/$cur $HOME/$cur
 done
 
 grep bash_profile_trick_skel $HOME/.bash_profile &> /dev/null
@@ -26,7 +26,7 @@ if [ $? -ne 0 ]; then
     echo "Installing bash_profile hook..."
     echo "
 # trick's bash_profile script
-source ~/.bash_profile_trick_skel" >> ~/.bash_profile
+source $HOME/.bash_profile_trick_skel" >> $HOME/.bash_profile
 fi
 
 echo "Done."
