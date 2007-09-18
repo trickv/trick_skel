6b486560593c9379825a8375976619513c9d36f2
Trick van Staveren <trick@vanstaveren.us>
2007-09-18 19:41:35 +0000

    hacks to work around various limitations within cron
    git-svn-id: http://svn.vanstaveren.us/trick_skel@194 434ebc3a-ad12-0410-8bf5-d84fc7825958

--- mintel_push.sh
+++ mintel_push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ssh willow rm -rf ~/src/trick_skel
-scp -r `pwd` willow:~/src/trick_skel
+ssh -i ~/.ssh/id_rsa willow rm -rf ~/src/trick_skel
+scp -i ~/.ssh/id_rsa -r `pwd` willow:~/src/trick_skel
 
-ssh doobie rm -rf ~/src/trick_skel
-scp -r `pwd` doobie:~/src/trick_skel
+ssh -i ~/.ssh/id_rsa doobie rm -rf ~/src/trick_skel
+scp -i ~/.ssh/id_rsa -r `pwd` doobie:~/src/trick_skel
