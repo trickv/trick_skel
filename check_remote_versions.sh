4ab88ce4874dec529d8eaf2e3ae7a726b9a7f032
Trick van Staveren <trick@vanstaveren.us>
2008-01-20 03:02:23 +0000

    add simple script to check remote versions
    git-svn-id: http://svn.vanstaveren.us/trick_skel@349 434ebc3a-ad12-0410-8bf5-d84fc7825958

---
+++ check_remote_versions.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cmd="cat .trick_skel/info/txt | grep 'Last Changed Rev'"
+
+HOME_HOSTS="trick@sting root@sting trick@mc root@mc pv@atlas root@atlas trick@sam root@sam"
+WORK_HOSTS="pv@gasket pv@raquel pv@baldrick pv@darth pv@willow"
+
+case "$1" in
+    work) HOST_LIST=$WORK_HOSTS;;
+    home) HOST_LIST=$HOME_HOSTS;;
+    *) echo "Sorry, you need to specify a host list (home or work)."; exit 1;;
+esac
+
+for host in $HOST_LIST; do
+    echo -n $host ' '
+    ssh $host "${cmd}"
+done
+
