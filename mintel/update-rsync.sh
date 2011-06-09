57be6e6c047eb13aeff05abc509128c3d1974931
Trick van Staveren <trick@vanstaveren.us>
2011-06-09 06:54:08 +0000

    trying to reorg
    git-svn-id: http://svn.vanstaveren.us/trick_skel@828 434ebc3a-ad12-0410-8bf5-d84fc7825958

---
+++ mintel/update-rsync.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync rsync://elecktra.shanghai.mintel.ad:49876/trick_skel/ "$HOME/src/trick_skel" -a --delete
--- update-mintel-rsync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rsync rsync://daredevil.shanghai.mintel.ad:49876/trick_skel/ "$HOME/src/trick_skel" -a --delete
