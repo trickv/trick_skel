85088ff8cdf4136a0008d5e94cfe01ad10eb46d0
Trick van Staveren <trick@vanstaveren.us>
2012-01-06 03:58:39 +0000

    huangniu is the new elecktra
    git-svn-id: http://svn.vanstaveren.us/trick_skel@862 434ebc3a-ad12-0410-8bf5-d84fc7825958

--- mintel/update-rsync.sh
+++ mintel/update-rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync rsync://elecktra.shanghai.mintel.ad:49876/trick_skel/ "$HOME/src/trick_skel" -a --delete
+rsync rsync://huangniu.shanghai.mintel.ad:49876/trick_skel/ "$HOME/src/trick_skel" -a --delete
