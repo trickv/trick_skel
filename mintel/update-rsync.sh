359acc68d5d682602f1227ffa6a54919f1d316da
Trick van Staveren <trick@vanstaveren.us>
2012-01-06 04:11:01 +0000

    Add option for setting server name, to make future changes like this a bit easier.
    git-svn-id: http://svn.vanstaveren.us/trick_skel@864 434ebc3a-ad12-0410-8bf5-d84fc7825958

--- mintel/update-rsync.sh
+++ mintel/update-rsync.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-rsync rsync://huangniu.shanghai.mintel.ad:49876/trick_skel/ "$HOME/src/trick_skel" -a --delete
+if [ "$1" != "" ]; then
+    host=$1
+else
+    host=huangniu
+fi
+
+rsync rsync://$host.shanghai.mintel.ad:49876/trick_skel/ "$HOME/src/trick_skel" -a --delete
