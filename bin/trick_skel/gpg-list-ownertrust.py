2a05c550c51199493a32bcb120a5521cffaaf89d
Patrick van Staveren <trick@vanstaveren.us>
2019-07-19 10:38:51 -0500

    add i3 disable-standby upstream script (not plumbed in), and add gnupg list ownertrust script

---
+++ bin/trick_skel/gpg-list-ownertrust.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import gnupg
+
+
+TRUST_LEVEL_MAP = {
+    'q': '???',
+    'n': 'Not',
+    'm': 'Marginal',
+    'f': 'Full',
+    'u': 'Ultimate'
+}
+
+
+def main():
+
+    gpg = gnupg.GPG()
+    gpg.encoding = 'utf-8'
+
+    keys = gpg.list_keys()
+    trust_keys = filter(lambda k: k['ownertrust'] != '-', keys)
+
+    for key in trust_keys:
+        trust_level = TRUST_LEVEL_MAP[key['ownertrust']]
+        print('{}  {:<59}  {}'.format(key['keyid'], key['uids'][0], trust_level))
+
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+
+    sys.exit(main())
