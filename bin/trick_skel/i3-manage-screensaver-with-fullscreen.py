1806bbc746731000ce70c1b604dc5b918b92d424
Patrick van Staveren <trick@vanstaveren.us>
2020-01-29 12:26:42 -0600

    keep track of state in memory and only execute commands if the state has really changed.

--- bin/trick_skel/i3-manage-screensaver-with-fullscreen.py
+++ bin/trick_skel/i3-manage-screensaver-with-fullscreen.py
@@ -7,6 +7,12 @@ import functools
 
 print = functools.partial(print, flush=True)
 
+# state True means DPMS should be on, and xautolock enabled
+# This happens when there is no fullscreen app to inhibit
+#   screen timeouts / screensavers.
+# We can safely assume this is true as this is launched at i3 startup.
+current_state = True
+
 i3 = i3ipc.Connection()
 
 parser = ArgumentParser(prog='disable-standby-fs',
@@ -23,8 +29,12 @@ def find_fullscreen(con):
     return [c for c in con.descendents() if c.type == 'con' and c.fullscreen_mode]
 
 
-def set_dpms(state):
-    if state:
+def set_dpms(new_state):
+    if new_state == current_state:
+        print("no-op: keeping existing state of {}".format(new_state))
+        return
+    current_state = new_state
+    if new_state:
         print('setting dpms on and enabling xautolock')
         call(['xautolock', '-enable'])
         call(['xset', '+dpms'])
@@ -35,10 +45,12 @@ def set_dpms(state):
 
 
 def on_fullscreen_mode(i3, e):
+    print("arr")
     set_dpms(not len(find_fullscreen(i3.get_tree())))
 
 
 def on_window_close(i3, e):
+    print("yarr")
     if not len(find_fullscreen(i3.get_tree())):
         set_dpms(True)
 
@@ -46,4 +58,8 @@ def on_window_close(i3, e):
 i3.on('window::fullscreen_mode', on_fullscreen_mode)
 i3.on('window::close', on_window_close)
 
+print("setup complete")
+
 i3.main()
+
+print("ok bye")
