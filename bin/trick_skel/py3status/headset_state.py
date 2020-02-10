88532fb80c6a8049a8c2dd77dad23a2fa2c1f9ee
Patrick van Staveren <trick@vanstaveren.us>
2020-02-10 12:02:07 -0600

    plumb in py version of headset state

--- bin/trick_skel/py3status/headset_state.py
+++ bin/trick_skel/py3status/headset_state.py
@@ -1,10 +1,24 @@
-import subprocess
+import pulsectl
 
 class Py3status:
+    pulse = pulsectl.Pulse('headsetstate')
 
-    def headset_state(self):
-        mic = subprocess.run("pacmd list-sources 1 | grep 'active port' | cut -d- -f3- | cut -d\> -f1", shell=True, capture_output=True, encoding='latin-1')
+    def headset_state2(self):
+        sources = self.pulse.source_list()
+        builtin_source = None
+        for source in sources:
+            if source.description == "Built-in Audio Analog Stereo":
+                builtin_source = source
+        if not builtin_source:
+            return {'full_text': "bug: built-in card not found"}
+        active = builtin_source.port_active
+        if not active:
+            port = "bug: not active?"
+        else:
+            port = active.name.replace("analog-input-", "")
+        card = builtin_source.description.split(' ')[0]
+        message = "{}: {}".format(card, port)
         return {
-            'full_text': mic.stdout.strip(),
+            'full_text': message,
             'cached_until': self.py3.time_in(1)
         }
