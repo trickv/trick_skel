a7630a4f4dca1af779681418347bc34948aede1a
Patrick van Staveren <trick@vanstaveren.us>
2023-02-14 08:29:26 -0600

    hack to repoint hotkey to the current layout

---
+++ .screenlayout/office-dual1080-lightning-tinnon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-1-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --off
+
+cd $(dirname $(realpath $0))
+pwd
+rm hotkey.sh
+ln -s $(basename $(realpath -P $0)) hotkey.sh
