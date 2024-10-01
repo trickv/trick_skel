b476ffa4db6f8f042f90ab0fdf3078143266c7d7
Patrick van Staveren <trick@vanstaveren.us>
2024-09-30 21:14:44 -0500

    add meraki wifi status from February 27, 2024

---
+++ bin/trick_skel/py3status/meraki_wifi_my.py
@@ -0,0 +1,41 @@
+import subprocess
+import pathlib
+import shlex
+import sys
+
+class Py3status:
+
+    def meraki_wifi_my(self):
+        #cmd = "curl -s http://my.meraki.com/index.json | jq -r \".config.node_name, .client.rssi\""
+        #cmd = "curl -s http://my.meraki.com/index.json | jq -r '.config.node_name'"
+        is_on_mintel_wifi_cmd = "nmcli d show wlp2s0  | grep GENERAL.CONNECTION | grep Mintel_Chicago_wifi"
+        p = subprocess.run(is_on_mintel_wifi_cmd, shell=True, cwd=pathlib.Path.home(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if p.returncode == 0:
+            cmd = "curl -s http://my.meraki.com/index.json | jq -r '.config.node_name + \" \" + .client.rssi + \"dB\"'"
+            #cmd = "echo hi"
+            #print(cmd, file=sys.stderr)
+            p = subprocess.run(cmd, cwd=pathlib.Path.home(), capture_output=True, shell=True)
+            #print(dir(p))
+            status = p.stdout.decode("latin9").strip()
+            #status = "foo"
+            message = "{}".format(status)
+            #message = "foo"
+            #print(message)
+            color = "#00ff00"
+        else:
+            message = " "
+            color = "#666666"
+        return {
+            'full_text': message,
+            'color': color,
+            'cached_until': self.py3.time_in(1)
+        }
+            
+
+
+if __name__ == "__main__":
+    """
+    Run module in test mode.
+    """
+    from py3status.module_test import module_test
+    module_test(Py3status)
