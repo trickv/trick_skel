eaee9012d4d7ca766ad34cf9256b589b9b254e24
Patrick van Staveren <trick@vanstaveren.us>
2024-11-04 14:41:08 -0600

    fix for new laptop

--- bin/trick_skel/py3status/meraki_wifi_my.py
+++ bin/trick_skel/py3status/meraki_wifi_my.py
@@ -8,7 +8,7 @@ class Py3status:
     def meraki_wifi_my(self):
         #cmd = "curl -s http://my.meraki.com/index.json | jq -r \".config.node_name, .client.rssi\""
         #cmd = "curl -s http://my.meraki.com/index.json | jq -r '.config.node_name'"
-        is_on_mintel_wifi_cmd = "nmcli d show wlp2s0  | grep GENERAL.CONNECTION | grep Mintel_Chicago_wifi"
+        is_on_mintel_wifi_cmd = "nmcli d show wlp0s20f3 | grep GENERAL.CONNECTION | grep Mintel_Chicago_wifi"
         p = subprocess.run(is_on_mintel_wifi_cmd, shell=True, cwd=pathlib.Path.home(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if p.returncode == 0:
             cmd = "curl -s http://my.meraki.com/index.json | jq -r '.config.node_name + \" \" + .client.rssi + \"dB\"'"
@@ -23,7 +23,7 @@ class Py3status:
             #print(message)
             color = "#00ff00"
         else:
-            message = " "
+            message = "(not meraki)"
             color = "#666666"
         return {
             'full_text': message,
