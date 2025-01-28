557e1d774aefb49c96328888af9ad1765cedb9e1
Patrick van Staveren <trick@vanstaveren.us>
2025-01-28 16:50:58 -0600

    add office 2.4 mac

--- bin/trick_skel/py3status/meraki_wifi_my.py
+++ bin/trick_skel/py3status/meraki_wifi_my.py
@@ -25,8 +25,26 @@ class Py3status:
             color = "#00ff00"
             timeout = 1
         else:
-            message = "(not meraki)"
-            color = "#666666"
+            is_on_home_wifi_cmd = "nmcli d show wlp0s20f3 | grep GENERAL.CONNECTION | grep rumbledethumps"
+            p = subprocess.run(is_on_home_wifi_cmd, shell=True, cwd=pathlib.Path.home(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if p.returncode == 0:
+                ap_mac_cmd = 'iwconfig wlp0s20f3 | grep "Access Point" | cut -d: -f4-'
+                p = subprocess.run(ap_mac_cmd, cwd=pathlib.Path.home(), capture_output=True, shell=True)
+                ap_mac = p.stdout.decode("latin9").strip()
+                ap_mac_map = {
+                    'FC:EC:DA:B8:5A:C3': 'Office',
+                    '74:AC:B9:28:E5:EE': 'Attic',
+                    'FC:EC:DA:B7:5A:C3': 'Office 2.4ghz',
+                }
+                if ap_mac in ap_mac_map:
+                    message = ap_mac_map[ap_mac]
+                else:
+                    message = "{}".format(ap_mac)
+                color = "#0000ff"
+                timeout = 1
+            else:
+                message = "(other network)"
+                color = "#666666"
         return {
             'full_text': message,
             'color': color,
